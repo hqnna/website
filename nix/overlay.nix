c6509f470260c3e34b1bf9bad180690efbaa1b87
hanna <me@hanna.lol>
2025-07-17 17:34:36 -0400

    build: update zine to latest stable version

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,13 +1,13 @@
 new: old: {
   zine-ssg = old.stdenv.mkDerivation (self: {
     pname = "zine-ssg";
-    version = "0.10.3";
+    version = "0.10.4";
     doBuild = false;
     doCheck = false;
 
     src = builtins.fetchTarball {
       url = "https://github.com/kristoff-it/zine/releases/download/v${self.version}/x86_64-linux-musl.tar.xz";
-      sha256 = "1y7dklv4mg4z8mbk564zjsiqfiincrjnalsyh18rn5aj46iwr7iw";
+      sha256 = "0p4f3728q9v2i424mqsc8ixi4gphzfk7z493skz5fzwfrcr6wqdw";
     };
 
     installPhase = ''
