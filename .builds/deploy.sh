3c277f90cbb1258928cca0812f7d69676c20c1a2
hanna <me@hanna.lol>
2025-08-17 12:48:58 -0400

    ci: setup workflow to build and deploy

---
+++ .builds/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id) \
+CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token) \
+npx wrangler@latest pages deploy public --project-name=website
