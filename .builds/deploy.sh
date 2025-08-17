c7173135ec34db96c88b3770afc84d104a23eb44
hanna <me@hanna.lol>
2025-08-17 17:16:00 -0400

    ci: deploy to production not preview

--- .builds/deploy.sh
+++ .builds/deploy.sh
@@ -2,4 +2,7 @@
 
 CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id) \
 CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token) \
-npx wrangler@latest pages deploy public --project-name=website
+npx wrangler@latest pages deploy public \
+  --project-name=website \
+  --commit-dirty \
+  --production
