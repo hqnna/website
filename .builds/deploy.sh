e95c8e59fec453d3c291386ec36192c8aca3890d
hanna <me@hanna.lol>
2025-08-18 10:55:28 -0400

    ci: clean up build manifest slightly

--- .builds/deploy.sh
+++ .builds/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id) \
-CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token) \
+CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id)
+CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token)
+
 npx wrangler@latest pages deploy public \
   --project-name=website \
   --commit-dirty=true \
