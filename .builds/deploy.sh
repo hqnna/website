48a8bab219eb674de652aff1e73a867ad406773e
hanna <me@hanna.lol>
2025-08-23 17:21:05 -0400

    ci: make sure to export credentials for wrangler

--- .builds/deploy.sh
+++ .builds/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id)
-CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token)
+export CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id)
+export CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token)
 
 npx wrangler@latest pages deploy public \
   --project-name=website \
