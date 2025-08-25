31043599642e41817d74a8da2dba434fc03cee93
hanna <me@hanna.lol>
2025-08-25 18:42:47 -0400

    chore: remove sourcehut build manifests

--- .builds/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export CLOUDFLARE_ACCOUNT_ID=$(cat /tmp/cloudflare_id)
-export CLOUDFLARE_API_TOKEN=$(cat /tmp/cloudflare_token)
-
-npx wrangler@latest pages deploy public \
-  --project-name=website \
-  --commit-dirty=true \
-  --branch=main
