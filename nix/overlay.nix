c28c6bce81bb19cd02fa95afd8a23a3192d0367f
hanna <me@hanna.lol>
2025-09-10 13:22:13 -0400

    build: update zine overlay to work with lix

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,17 +2,17 @@ new: old: {
   zine-ssg = old.stdenv.mkDerivation (self: {
     pname = "zine-ssg";
     version = "0.11.1";
-    doBuild = false;
-    doCheck = false;
+
+    phases = [ "installPhase" ];
 
     src = builtins.fetchTarball {
       url = "https://github.com/kristoff-it/zine/releases/download/v${self.version}/x86_64-linux-musl.tar.xz";
-      sha256 = "04kcx1j1wh3p0nd5lfc8hdhn29x9x4cxfh3c7zvi8n2kbq99m8c7";
+      sha256 = "krPomEDxKktsYnhSSAO9+OGA8UeMrgN3gNpKQr5v5yA=";
     };
 
     installPhase = ''
       runHook preInstall
-      install -m755 -D zine $out/bin/zine
+      install -m755 -D $src $out/bin/zine
       runHook postInstall
     '';
   });
